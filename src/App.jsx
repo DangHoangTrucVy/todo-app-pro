9ac43a34d7a52be63bcd8d98915bf687523aa86f
DangHoangTrucVy <danghoangtrucvy090105@gmail.com>
2026-04-05 20:21:22 +0700

    first commit: todo app setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import Header from './components/Header'
+function App() {
+ 
+
+  return (
+   <div className='app'>
+    <div className='container'>
+      <Header />
+    </div>
+   </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>To-do App Pro</h1>
+      <p>Quản lý công việc hằng ngày</p>
+    </header>
+  );
+}
+
+export default Header;
